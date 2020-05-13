844d7d1fa3a30441a43a680de4940a402d9e1333
jeliel <jeliel.augusto10@gmail.com>
2020-05-12 21:17:26 -0400

    All files

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { Provider } from 'react-redux';
+import productsReducer from "./store/reducers/products";
+import ShopNavigator from "./navigation/ShopNavigator";
+import ReduxThunk from 'redux-thunk'
+import * as Font from 'expo-font';
+import { AppLoading } from "expo";
+import cartReducer from "./store/reducers/cart";
+import ordersReducer from "./store/reducers/orders";
+import authReducer from './store/reducers/auth';
+import auth from "./store/reducers/auth";
+import NavigationContainer from "./navigation/NavigationContainer";
+const rootReducer = combineReducers({
+    products: productsReducer,
+    cart: cartReducer,
+    orders: ordersReducer,
+    auth: authReducer
+})
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+const fetchFonts = () => Font.loadAsync(
+    {
+      'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+})
+export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+  if(!fontLoaded){
+    return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)}/>
+  }
+  return (
+      <Provider store={store}>
+        <NavigationContainer />
+      </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
